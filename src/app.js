3a7a9b2c4b2734d6aeed99af92da51890ae2b0f1
vikashkumar6207 <136398175+vikashkumar6207@users.noreply.github.com>
2024-05-29 21:43:47 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+// Write a GET route to return the value stored in the NUMBER variable stored in the .env file
+/*
+{
+    "number" : "2"
+}
+*/
+app.get('/api/get-env', (req, res) => {
+   //Write your code here
+});
+
+module.exports = app;
